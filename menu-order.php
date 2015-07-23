70ee7b84682decd09bc5852e19034b523190a45d
Kim Brose <kim.brose@rwth-aachen.de>
2015-07-23 06:33:36 +0200

    create example

---
+++ menu-order.php
@@ -0,0 +1,20 @@
+<?php
+
+$home=array(
+	"about"=>0,
+	"dates"=>1,
+	"contact"=>2,
+	"legal"=>3,
+	"404"=>-1,
+	"hidden"=>-1,
+);
+
+$about=array(
+	"me"=>0,
+	"company"=>1,
+	"partners"=>2
+);
+
+$index=array("home"=>$home, "about"=>$about);
+
+?>
